0e967fb921bf67a78e57fdec7b80bf10b8016805
Akshat Jha <akshatjha27@gmail.com>
2025-06-26 11:34:25 +0530

    Updated Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -42,7 +42,7 @@ export default function Hero() {
           </motion.div>
 
           {/* Title and Description - Positioned lower with clear separation */}
-          <div className="mt-80 sm:mt-72 md:mt-80 lg:mt-130">
+          <div className="mt-80 sm:mt-72 md:mt-80 lg:mt-200">
 
           <motion.p
             initial={{ opacity: 0, y: 20 }}
